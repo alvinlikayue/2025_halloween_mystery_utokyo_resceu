82f3620f7d4ed15385747011e460fd675758dc5c
alvin.li <alvin.li@ligo.org>
2025-10-28 11:56:49 +0900

    update index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-function startMystery() {
-  window.location.href = "puzzle1.html";
-}
+// 星が少し動く演出
+const stars = document.querySelector('.stars');
+let move = 0;
+setInterval(() => {
+  move += 1;
+  stars.style.backgroundPosition = `${move}px ${move}px`;
+}, 50);
+
+// ロック機能（例：puzzle7 は開かない）
+document.querySelectorAll('.locked').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    alert("この謎はまだ解放されていません。全ての謎を解くと現れます！");
+  });
+});
+
